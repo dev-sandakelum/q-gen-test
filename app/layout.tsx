e0f9e6203384b4a519d3179a23745c0a6cd62c09
dev.sandakelum <dev.sandakelum@example.com>
2025-12-04 11:48:54 +0530

    g

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Learning Hub",
+  title: "Q-Gen testing",
   description: "A hub for all your learning resources",
 };
 
